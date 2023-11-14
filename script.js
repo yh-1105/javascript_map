a3c31daa2f9dd9e95affd9747d6288b53c6b92a6
yh-1105 <107912096+yh-1105@users.noreply.github.com>
2023-11-15 01:28:26 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript
-
-console.log('Hello world!');
-
-var map = L.map('map').setView([33.671348877245634, 130.44442368478892], 15);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
